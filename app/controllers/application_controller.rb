3ae97f2d06b089e4f011cb56ec4bba697f877444
zeal <zealav@gmail.com>
2012-11-11 18:10:23 +0600

    static

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  @users = User.all
+  @microposts = Micropost.all
+
   protect_from_forgery
+
+
 end
--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,6 @@
 class RootController < ApplicationController
   def index
-
+    @users = User.all
+    @microposts = Micropost.all
   end
 end
